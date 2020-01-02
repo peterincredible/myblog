5617b58f2804b4a8cf331b74e936e2c828b47281
peter dibie <ekene.dibie@gmail.com>
2020-01-02 11:02:37 +0100

    another update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/user/api",userrouter);
 app.use("/admin/api",postrouter);
 
  if(process.env.NODE_ENV == "production"){
-     mongoose.connect('mongodb://peterincredible:omolola-93@ds259528.mlab.com:59528/heroku_3mn5n4k7', {useNewUrlParser: true});
+    // mongoose.connect('mongodb://peterincredible:omolola-93@ds259528.mlab.com:59528/heroku_3mn5n4k7', {useNewUrlParser: true});
      app.use(express.static("my-blog/build"));
      app.get("*",(req,res)=>{
          res.sendFile(path.resolve(__dirname,"my-blog","build","index.html"))
@@ -22,9 +22,9 @@ app.use("/admin/api",postrouter);
     mongoose.connect('mongodb://localhost/react-blog', {useNewUrlParser: true});
  }
  mongoose.connection.once("open",()=>{
-    console.log("the database is open and working perfectly");
+    console.log("the database is open andddd working perfectly");
 })
  let port = process.env.PORT || 5000;
 app.listen(port,()=>{
-    console.log("server is listening to port ", port);
+    console.log("server is listeningjjjj to port ", port);
 });
